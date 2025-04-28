5a4b6a63963596bc5db5e7c3dd408a9c03dd2b3c
diegoalss <diegoalessandross210207@gmail.com>
2025-04-28 19:40:12 +0000

    criei sequencias de scanf e printf

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,6 +7,72 @@
 //Teste larissa
 
 int main() {
+    #include <stdio.h>
+    char codigo1[5];
+    int populacao1;
+    float area1;
+    float pib1;
+    int turisticos1;
+
+    char codigo2[5];
+    int populacao2;
+    float area2;
+    float pib2;
+    int turisticos2;
+
+    // Cadastro da primeira carta
+    printf("Cadastro da primeira carta:\n");
+    printf("Digite o codigo da carta (ex: A01): ");
+    scanf("%s", codigo1);
+    
+    printf("Digite a populacao: ");
+    scanf("%d", &populacao1);
+    
+    printf("Digite a area: ");
+    scanf("%f", &area1);
+    
+    printf("Digite o PIB: ");
+    scanf("%f", &pib1);
+    
+    printf("Digite o numero de pontos turisticos: ");
+    scanf("%d", &turisticos1);
+
+    // Cadastro da segunda carta
+    printf("\nCadastro da segunda carta:\n");
+    printf("Digite o codigo da carta (ex: B02): ");
+    scanf("%s", codigo2);
+    
+    printf("Digite a populacao: ");
+    scanf("%d", &populacao2);
+    
+    printf("Digite a area: ");
+    scanf("%f", &area2);
+    
+    printf("Digite o PIB: ");
+    scanf("%f", &pib2);
+    
+    printf("Digite o numero de pontos turisticos: ");
+    scanf("%d", &turisticos2);
+
+    // Exibição dos dados
+    printf("\n--- Dados das Cartas ---\n");
+
+    printf("\nCarta 1:\n");
+    printf("Codigo: %s\n", codigo1);
+    printf("Populacao: %d habitantes\n", populacao1);
+    printf("Area: %.2f km²\n", area1);
+    printf("PIB: %.2f reais\n", pib1);
+    printf("Pontos Turisticos: %d\n", turisticos1);
+
+    printf("\nCarta 2:\n");
+    printf("Codigo: %s\n", codigo2);
+    printf("Populacao: %d habitantes\n", populacao2);
+    printf("Area: %.2f km²\n", area2);
+    printf("PIB: %.2f reais\n", pib2);
+    printf("Pontos Turisticos: %d\n", turisticos2);
+
+
+
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
     
@@ -19,4 +85,6 @@ int main() {
     // Exiba os valores inseridos para cada atributo da cidade, um por linha.
 
     return 0;
+
+
 }
